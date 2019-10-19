f96b81af9ebdd9472be1337dd4b63f2330fb2dbf
Techworker <benjaminansbach@gmail.com>
2019-10-20 00:59:23 +0200

    Added google analytics

--- public/include/head.php
+++ public/include/head.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="<?=$_t['lang']['iso_lang']?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150402502-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-150402502-1');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Benjamin Ansbach">
